0545c47e1e89c4c630932fa8583b06a0140f3979
code-for-coffee <jtraverjr@gmail.com>
2016-09-25 17:44:45 -0500

    end of HoF Practice && week 2

--- wk2/ArrayHelper.scala
+++ wk2/ArrayHelper.scala
@@ -33,6 +33,15 @@ object ArrayHelper {
     go(0)
   }
 
+  def curry[A,B,C](f: (A,B) => C): A => (B => C) =
+    a => b => f(a, b)
+
+  def uncurry[A,B,C](f: A => B => C): (A,B) => C =
+    (a,b) => f(a)(b)
+
+  def compose[A,B,C](f: B => C, g: A => B): A => C =
+    a => f(g(a))
+
   def main(args: Array[String]): Unit = {
     var names = Array("bob", "magda", "beth", "marshmello")
     println(findFirstString(names, "beth"))
